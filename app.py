eb6be16170a574cfab59d7e663634061f926e8f7
lokeshkumarVis <viswavarapu.lokesh@gmail.com>
2018-08-08 09:28:44 -0500

    microservice property repo

--- app.py
+++ app.py
@@ -1,35 +1,50 @@
-import sys, os
-
-## Reis api commented
+import sys, os, urllib.parse
+import uuid
+## Reis api is commented from use
 '''
 from ReisApi import ReisApi
 from ReisApi import ReisCompRent
 from ReisApi import ReisCompSale
-'''
-import json
 import psycopg2
 import psycopg2.extras
-from flask import Flask, render_template, Response, redirect,url_for,jsonify
+'''
+import json
+
+from flask import Flask, render_template, Response, redirect,url_for,jsonify, request
 import time
 from concurrent.futures import ThreadPoolExecutor, wait, as_completed
-from flask import request
 import requests
 
 app = Flask(__name__)
 
 @app.route('/',methods = ['POST', 'GET'])
 def index():
-	return 'This is Database Service home'#render_template('index.html')
+	#return 'This is Addres validation Service home'
+	return render_template('index.html')
 
 @app.route('/property',methods = ['POST', 'GET'])
-def database():
+def address():
 	if request.method == 'POST':
-		req_data = dict(request.form)#request.args.get('address')
-		req_address=req_data["address"]
-		address={'sent_address':req_data["address"], 'recieved_address':req_data["address"],'Service_Called':' property service'}
+		if request.form:
+			req_data = dict(request.form)#request.args.get('address')
+			req_address=req_data["address"]
+			req_address=req_address[0]
+			address=parse_address(req_address)
+		elif (request.get_json())!=None:
+			req_address = request.get_json()
+			address=req_address
+		address={'Request':req_address, 'parsed_address':address,'property guid':uuid.uuid4(),'Service_Called':'address validation service'}
 		return jsonify(address)#str(req_data)#
 	elif request.method == 'GET':
+		address=request.args.get("address")
+		if address!= None:	
+			print(address)
 		return render_template('index.html')
+def parse_address(address):
+	print(address)
+	address=address.split(" ")
+	address={"StreetAddress": ' '.join(address[:-2]),"CityName": address[-2],"StateCode": address[-1]}
+	return address
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80, debug=True, threaded=True)
